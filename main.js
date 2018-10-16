bfde5f13e0fe5a014e3c3a6530f8353b4df85d89
FranzCSUF <fsalvador79@gmail.com>
2018-10-15 17:25:20 -0700

    Added printer error algorithm challenge.

--- main.js
+++ main.js
@@ -396,3 +396,41 @@ const reverseSeq = ranNum => {
 }
 
 // console.log(reverseSeq(5))
+
+// Printer Errors
+// The colors used by the printer are recorded in a control string. For example
+// a "good" control string would be aaabbbbhaijjjm meaning that the printer used
+// three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad"
+// control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
+
+// You have to write a function printer_error which given a string will output
+// the error rate of the printer as a string representing a rational whose numerator
+// is the number of errors and the denominator the length of the control string.
+// Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// sort string - letters after m are errors.
+// correct = lastIndexOf m + 1
+// errors = length - correct / length
+
+function printerError(s) {
+  let errors = 0
+  if (s.length - 1 !== 'm') {
+    errors = s.length - ((s.split('').sort().lastIndexOf('m')) + 1)
+  }
+  return errors + '/' + s.length
+}
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+// console.log(printerError('aaaxbbbbyyhwawiwjjjwwm'))
+
+// Optimal Solution using Regxx
+// function printerError(s) {
+//   return s.match(/[^a-m]/g).length + "/" + s.length;
+// }
