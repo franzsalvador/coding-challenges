596512a2a7951942c659333a77b013dfa7e297ed
FranzCSUF <fsalvador79@gmail.com>
2018-10-24 23:12:31 -0700

    Adding exes and ohs coding exercise.

--- main.js
+++ main.js
@@ -845,9 +845,42 @@ function descendingOrder(n) {
   return parseInt(n.toString().split('').sort().reverse().join(''), 10)
 }
 
-console.log(descendingOrder(0))
-// 0
-console.log(descendingOrder(1))
-// 1
-console.log(descendingOrder(123456789))
-// 987654321
+// console.log(descendingOrder(0))
+// // 0
+// console.log(descendingOrder(1))
+// // 1
+// console.log(descendingOrder(123456789))
+// // 987654321
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  const arr = str.toLowerCase().split('')
+  let x = 0
+  let o = 0
+  arr.forEach(letter => {
+    if (letter === 'x') x++
+    else if (letter === 'o') o++
+  })
+  if (x === 0 && o === 0) return true
+  else if (x === o) return true
+  else return false
+}
+
+console.log(XO('xo'))
+// ,true);
+console.log(XO('xxOo'))
+// ,true);
+console.log(XO('xxxm'))
+// ,false);
+console.log(XO('Oo'))
+// ,false);
+console.log(XO('ooom'))
+// ,false);
