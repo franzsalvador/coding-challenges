fa02062de94e8bacf7ddf9eeab82fd680966a16c
FranzCSUF <fsalvador79@gmail.com>
2018-10-26 08:25:40 -0700

    Adding find outlier coding exercise.

--- main.js
+++ main.js
@@ -881,13 +881,51 @@ function XO(str) {
 //   return (x && x.length) === (o && o.length);
 // }
 
-console.log(XO('xo'))
-// ,true);
-console.log(XO('xxOo'))
-// ,true);
-console.log(XO('xxxm'))
-// ,false);
-console.log(XO('Oo'))
-// ,false);
-console.log(XO('ooom'))
-// ,false);
+// console.log(XO('xo'))
+// // ,true);
+// console.log(XO('xxOo'))
+// // ,true);
+// console.log(XO('xxxm'))
+// // ,false);
+// console.log(XO('Oo'))
+// // ,false);
+// console.log(XO('ooom'))
+// // ,false);
+
+// Find The Parity Outlier
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// function findOutlier(integers) {
+//   let odd = []
+//   let even = []
+//   integers.forEach(integer => {
+//     if (integer % 2 === 0) even.push(integer)
+//     else odd.push(integer)
+//   })
+//   if (odd.length === 1) return odd[0]
+//   else return even[0]
+// }
+
+function findOutlier(integers) {
+  const odd = integers.filter(int => int % 2 !== 0)
+  const even = integers.filter(int => int % 2 === 0)
+  return odd.length > even.length ? even[0] : odd[0]
+}
+
+console.log(findOutlier([0, 1, 2]))
+// , 1)
+console.log(findOutlier([1, 2, 3]))
+// , 2)
+console.log(findOutlier([2, 6, 8, 10, 3]))
+// , 3)
+console.log(findOutlier([0, 0, 3, 0, 0]))
+// , 3)
+console.log(findOutlier([1, 1, 0, 1, 1]))
+// , 0)
