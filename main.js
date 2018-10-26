f16854c950a136d6c0c8db8126bc7bf44040822d
FranzCSUF <fsalvador79@gmail.com>
2018-10-26 10:06:44 -0700

    Adding persistent bugger exercise.

--- main.js
+++ main.js
@@ -919,13 +919,53 @@ function findOutlier(integers) {
   return odd.length > even.length ? even[0] : odd[0]
 }
 
-console.log(findOutlier([0, 1, 2]))
-// , 1)
-console.log(findOutlier([1, 2, 3]))
-// , 2)
-console.log(findOutlier([2, 6, 8, 10, 3]))
-// , 3)
-console.log(findOutlier([0, 0, 3, 0, 0]))
-// , 3)
-console.log(findOutlier([1, 1, 0, 1, 1]))
-// , 0)
+// console.log(findOutlier([0, 1, 2]))
+// // , 1)
+// console.log(findOutlier([1, 2, 3]))
+// // , 2)
+// console.log(findOutlier([2, 6, 8, 10, 3]))
+// // , 3)
+// console.log(findOutlier([0, 0, 3, 0, 0]))
+// // , 3)
+// console.log(findOutlier([1, 1, 0, 1, 1]))
+// // , 0)
+
+// Write a function, persistence, that takes in a positive parameter num and returns
+// its multiplicative persistence, which is the number of times you must multiply the
+// digits in num until you reach a single digit.
+
+// For example:
+// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+// // and 4 has only one digit
+
+// persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//  // 1*2*6 = 12, and finally 1*2 = 2
+
+// persistence(4) === 0 // because 4 is already a one-digit number
+
+// convert number into single characters in an array
+// multiply numbers using
+// if num is one character, return num
+// if > 1 char, multiply
+// let measure = 0
+
+function persistence(num) {
+  let measure = 0
+  let digits = num.toString().split('')
+  let product
+  while (digits.length > 1) {
+    product = digits.reduce((a, b) => a * b)
+    measure++
+    digits = product.toString().split('')
+  }
+  return measure
+}
+
+console.log(persistence(39))
+// 3
+console.log(persistence(4))
+// 0
+console.log(persistence(25))
+// 2
+console.log(persistence(999))
+// 4
