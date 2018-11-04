affa4a57f307202f50ebc86bcaccd998042830f6
FranzCSUF <fsalvador79@gmail.com>
2018-11-04 12:53:06 -0800

    Adding digital root coding exercise.

--- main.js
+++ main.js
@@ -930,6 +930,7 @@ function findOutlier(integers) {
 // console.log(findOutlier([1, 1, 0, 1, 1]))
 // // , 0)
 
+// Persistent Bugger
 // Write a function, persistence, that takes in a positive parameter num and returns
 // its multiplicative persistence, which is the number of times you must multiply the
 // digits in num until you reach a single digit.
@@ -961,11 +962,66 @@ function persistence(num) {
   return measure
 }
 
-console.log(persistence(39))
-// 3
-console.log(persistence(4))
-// 0
-console.log(persistence(25))
-// 2
-console.log(persistence(999))
-// 4
+// console.log(persistence(39))
+// // 3
+// console.log(persistence(4))
+// // 0
+// console.log(persistence(25))
+// // 2
+// console.log(persistence(999))
+// // 4
+
+// // Sum of Digits / Digital Root
+// In this kata, you must create a digital root function.
+
+// A digital root is the recursive sum of all the digits in a number. Given n,
+// take the sum of the digits of n. If that value has two digits, continue
+// reducing in this way until a single-digit number is produced. This is only
+// applicable to the natural numbers.
+
+// Here's how it works:
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+// function digital_root(n) {
+//   let newArr = n.toString().split('').map(int => parseInt(int, 10))
+//   let sum = 0
+//   while (newArr.length > 1) {
+//     sum = newArr.reduce((a, b) => a + b)
+//     newArr = sum.toString().split('').map(int => parseInt(int, 10))
+//   }
+//   return sum
+// }
+
+function digitalRoot(n) {
+  n = n.toString().split('').map(int => parseInt(int, 10)).reduce((a, b) => a + b)
+  if (n > 9) {
+    return digitalRoot(n)
+  }
+  return n
+}
+
+console.log(digitalRoot(16))
+console.log(digitalRoot(456))
